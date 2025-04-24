82c60da481f94136399741488dbbc103b3c06cd2
Chetan Kumar <chetan.kumar@ericsson.com>
2025-04-24 13:23:21 +0530

    added numpy random, python optimization

--- Django/home/urls.py
+++ Django/home/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from home import views
 
+admin.site.site_header = "Cakadola Admin"
+admin.site.site_title = "Cakadola Admin Portal"
+admin.site.index_title = "Welcome to Cakadola Admin Portal"
+
 urlpatterns = [
     path("", views.index, name='home'),
     path("about", views.about, name='about'),
